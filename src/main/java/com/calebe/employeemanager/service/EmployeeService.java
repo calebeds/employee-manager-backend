864492d3a8866628b48a633938990cd89e3208bd
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-09-17 23:12:32 -0300

    add employee resource

--- src/main/java/com/calebe/employeemanager/service/EmployeeService.java
+++ src/main/java/com/calebe/employeemanager/service/EmployeeService.java
@@ -5,11 +5,13 @@ import com.calebe.employeemanager.model.Employee;
 import com.calebe.employeemanager.repo.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
+@Transactional
 public class EmployeeService {
     private final EmployeeRepo employeeRepo;
 
